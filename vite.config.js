7811f22611ee6b86629b7184b8798a7b7819af51
Subodh <sharma2000subodh@gmail.com>
2024-04-11 01:03:26 +0530

    change port

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 3000,
+		port: "https://chatapp-frontend-ruby.vercel.app",
 		proxy: {
 			"/api": {
 				target: "https://chatapp-server-lilac.vercel.app",
